2a74bbc342f0d10d2de5b5d55635b6bea609d4f6
Your Name <you@example.com>
2024-07-28 00:06:19 +0900

    投稿機能修正1

--- post/views.py
+++ post/views.py
@@ -1,52 +1,45 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, DetailView, ListView
+from django.views.generic.edit import CreateView
 from .models import Post, Like
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'post/post_list.html', {'posts': posts})
+class HomeView(LoginRequiredMixin, ListView):
+    template_name = "Posts/home.html"
+    # Postをモデルに指定
+    model = Post
+    # select_relatedを用いることで発行されたクエリによってユーザー情報とツイート情報がidをキーとして結合された状態のレコードが返ってくる
+    # そのため{{Post.user}}{{Post.content}},{{Post.created_at}}で再度クエリを発行する必要がなくなり、N+1問題を解決できる
+    queryset = Post.objects.select_related("user")
 
 
-@login_required
-def create_post(request):
-    if request.method == 'POST':
-        post = Post(user=request.user, content=request.POST['content'])
-        post.save()
-        return redirect('post_list')
-    return render(request, 'post/create.html')
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "Posts/create.html"
+    # Postモデルを指定
+    model = Post
+    # contentのみ表示
+    fields = ['content']
+    success_url = reverse_lazy('Posts:home')
+
+    def form_valid(self, form):
+        # フォームがエラーなく送信された際に発動するメソッド
+        # データの受取・保存の役割を担う
+        # self.request.userログインしているユーザー
+        # form.instance.user：フォームを送ったユーザー
+        # フォームを送ったユーザー情報にツイートボタンを押したユーザーを格納
+        form.instance.user = self.request.user
+        # super().で継承することでform_validのもともとの機能も保つ
+        return super().form_valid(form)
 
 
-def post_detail(request, pk):
-    """日記の詳細ページの表示"""
-    post = get_object_or_404(Post, pk=pk)
-    like = Like.objects.filter(post=post, user=request.user)
-    is_liked = like.exists()
-    like_count = Like.objects.filter(post=post).count()
-
-    context = {
-        'post': post,
-        'is_liked': is_liked,
-        'like_count': like_count,
-    }
-
-    return render(request, 'detail.html', context)
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = 'Posts/detail.html'
+    queryset = Post.objects.select_related("user")
 
 
-def like_post(request):
-    """日記のいいね処理"""
-    post_pk = request.POST.get('post_pk')#POSTメソッドのbodyに格納されているpost_pkを取得
-    post = get_object_or_404(Post, pk=post_pk)
-    like = Like.objects.filter(post=post, user=request.user)
-    context = {}
-    if like.exists():
-        like.delete()
-        context['method'] = 'delete'
-    else:
-        like.create(post=post, user=request.user)
-        context['method'] = 'create'
-    #該当の投稿のいいねの数を取得
-    context['like_count'] = Like.objects.filter(post=post).count()
-    return JsonResponse(context)
-
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    template_name = 'Posts/delete.html'
+    model = Post
+    success_url = reverse_lazy("Posts:home")
