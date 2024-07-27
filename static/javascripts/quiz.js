2f51316675a8e93e812e40200b0ecdf90ded7440
T.Yamaguchi <yamatatsu4680@gmail.com>
2024-07-27 18:53:32 +0900

    クエスチョンを正常に動作するように修正

--- static/javascripts/quiz.js
+++ static/javascripts/quiz.js
@@ -1,10 +1,22 @@
+function showNextQuiz(current, total) {
+    var currentQuiz = document.getElementById('quiz-' + current);
+    var nextQuiz = document.getElementById('quiz-' + (current + 1));
+    if (nextQuiz) {
+        currentQuiz.style.display = 'none';
+        nextQuiz.style.display = 'block';
+    }
+}
+
+function showBeforeQuiz(current, total) {
+    var currentQuiz = document.getElementById('quiz-' + current);
+    var beforeQuiz = document.getElementById('quiz-' + (current -1));
+    if (beforeQuiz) {
+        currentQuiz.style.display = 'none';
+        beforeQuiz.style.display = 'block';
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    let currentQuestionIndex = 0;
-    const questions = document.querySelectorAll('.quiz');
-    const nextButtons = document.querySelectorAll('.next-btn');
-    const prevButtons = document.querySelectorAll('.before-btn');
-    const submit = document.querySelectorAll(".submit-btn");
-    
     // ランダムにシャッフルする関数
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -26,41 +38,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     };
     //シャッフルここまで
-
-    function showQuestion(index) {
-        questions.forEach((question, i) => {
-            question.style.display = i === index ? 'block' : 'none';
-        });
-    }
-
-    nextButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            currentQuestionIndex++;
-            if (currentQuestionIndex >= questions.length) {
-                currentQuestionIndex = 0;
-            }
-            showQuestion(currentQuestionIndex);
-        });
-    });
-
-    prevButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            currentQuestionIndex--;
-            if (currentQuestionIndex < 0) {
-                currentQuestionIndex = questions.length - 1;
-            }
-            showQuestion(currentQuestionIndex);
-        });
-    });
-
-    // 初期表示
-    showQuestion(currentQuestionIndex);
-
-    //蛙化度計測ボタン用
-    submit.style.display = 'none';
-    if (currentQuestionIndex = questions.length) {
-        submit.style.display = 'block';
-    }
-
 
 });
